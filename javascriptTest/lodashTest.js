b554cd2198e01913ca1cd71ff977b86bf71a2bfd
yeonggi <epikhighjin@gmail.com>
2022-02-27 20:45:18 +0900

    Lodash Test

---
+++ javascriptTest/lodashTest.js
@@ -0,0 +1,14 @@
+var _ = require('lodash');
+
+
+const newList = [0, 1, 2, 3].filter((v, i) => {
+    return i % 2 == 0;
+})
+
+console.log(newList)
+
+let a = [1, 2, 3]
+a.map("3")
+
+
+
--- javascriptTest/momentTest.js
+++ javascriptTest/momentTest.js
@@ -1,6 +1,7 @@
 var moment = require("moment")
 
-let timeDiff = moment("2022-01-25")
-var duration = moment.duration(moment(moment("2022-01-25 12:00:00")).diff(new Date()))
+let timeDiff = new Date("2022-02-19 22:55:00")
+console.log(timeDiff)
+var duration = moment.duration(moment("2022-02-19 22:55:00").diff(new Date()))
 console.log(Math.floor(duration.asHours()) + ':' + duration.minutes() + ':' + duration.seconds())
-
+console.log(duration.asMinutes())
